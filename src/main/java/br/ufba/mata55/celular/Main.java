a5b98225ab3220d862ead5a3f420805399a5411e
Rodrigo Rocha <rodrigorgs@gmail.com>
2019-04-22 11:05:08 -0300

    Organiza melhor o código

--- src/main/java/br/ufba/mata55/celular/Main.java
+++ src/main/java/br/ufba/mata55/celular/Main.java
@@ -1,98 +1,12 @@
 package br.ufba.mata55.celular;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
-import javax.swing.event.MouseInputListener;
-
-class Painel extends JPanel implements KeyListener, MouseInputListener {
-	private static final long serialVersionUID = 1L;
-
-	public static final int LARGURA = 500;
-	public static final int ALTURA = 500;
-
-	private Terreno terreno;
-
-	public Painel() {
-		terreno = new Terreno(LARGURA, ALTURA);
-
-		addMouseListener(this);
-		addMouseMotionListener(this);
-	}
-
-	int x = 0;
-
-	public void atualiza() {
-		x++;
-	}
-
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-
-		g.setColor(Color.BLUE);
-		g.fillRect(x, 10, 20, 12);
-
-		terreno.desenha(g);
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		char keyChar = e.getKeyChar();
-
-		if (keyChar == 27) {
-			System.exit(0);
-		}
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-	}
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		Controle.mouseX = e.getX();
-		Controle.mouseY = e.getY();
-	}
-}
 
 public class Main extends JFrame {
 	private Painel painel;
